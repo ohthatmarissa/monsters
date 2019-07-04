494804ca3158c86764a73c90986d5fea2fdd3376
Marissa Perry <ohthatmarissa@gmail.com>
2019-07-03 17:54:33 -0700

    add search input

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,9 +20,15 @@ componentDidMount() {
 
 
 render() {
+  const { monsters, searchField } = this.state;
+  const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
   return (
     <div className="App">
-      <CardList monsters={this.state.monsters} />
+      <input type='search' 
+      placeholder='Search Monsters' 
+      onChange={e => this.setState({ searchField: e.target.value 
+      })} />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
