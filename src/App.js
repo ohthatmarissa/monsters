ce7b457e88581093e6d37766c8d16de8b0aa9da3
Marissa Perry <ohthatmarissa@gmail.com>
2019-07-03 18:59:54 -0700

    cleaned up the search code in render and search-box component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CardList, cardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -9,7 +10,7 @@ class App extends Component {
     this.state = {
       monsters: [],
       searchField: ''
-    };
+    }; 
   }
 
 componentDidMount() {
@@ -18,16 +19,20 @@ componentDidMount() {
   .then(users => this.setState({ monsters: users }));
 }
 
+handleChange = (e) => {
+  this.setState({ searchField: e.target.value 
+  })
+}
 
 render() {
   const { monsters, searchField } = this.state;
   const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
   return (
     <div className="App">
-      <input type='search' 
-      placeholder='Search Monsters' 
-      onChange={e => this.setState({ searchField: e.target.value 
-      })} />
+      
+      <SearchBox 
+      placeholder='Search Monsters'
+      handleChange={this.handleChange} />
       <CardList monsters={filteredMonsters} />
     </div>
   );
