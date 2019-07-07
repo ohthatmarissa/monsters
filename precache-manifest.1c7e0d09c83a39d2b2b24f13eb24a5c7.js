ddfa9bf19bb252dfd9a9374aaf8a07ee4e71298a
Marissa Perry <ohthatmarissa@gmail.com>
2019-07-07 13:16:37 -0700

    Updates

---
+++ precache-manifest.1c7e0d09c83a39d2b2b24f13eb24a5c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a1c5d7f711f76b5fe19b2165a1bfab",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "1af775d424f71f150e99",
+    "url": "/monsters/static/css/main.6add6005.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1af775d424f71f150e99",
+    "url": "/monsters/static/js/main.7fc6b784.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
